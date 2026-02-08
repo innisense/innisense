d23cc71e426d3a8c486c95bcb059c9fb8a843a62
innisense <thisisromchikv2@rambler.ru>
2026-02-08 13:47:30 +0300

    innisense.lua

--- innisense.lua
+++ innisense.lua
@@ -1,1 +1,12 @@
+ --[[
+  _             _                         
+ (_)           (_)                        
+  _ _ __  _ __  _ ___  ___ _ __  ___  ___ 
+ | | '_ \| '_ \| / __|/ _ \ '_ \/ __|/ _ \
+ | | | | | | | | \__ \  __/ | | \__ \  __/
+ |_|_| |_|_| |_|_|___/\___|_| |_|___/\___|
+
+plz dont skid this skibidy poopity poo script
+
+]]
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/89b00f515844cd3d6e30942afce718939cf76d1a862edb0660c67839893dd5c9/download"))()
